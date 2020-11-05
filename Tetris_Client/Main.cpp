75526cc8807275a3c3f96d4e859fff9eddac7893
wavem <fenix24@naver.com>
2020-11-05 10:13:05 +0900

    [New] Create Grid for Real Gaming
    - XE5 에서 만들어 놓은, 기본 게임판 등을 가져왔다.
    - 그리드 같은게 그냥 복사 붙여넣기가 되더라.
    - 내부 소스는 다음에 가져오자.
    - 끝. 커밋하겠음.

--- Tetris_Client/Main.cpp
+++ Tetris_Client/Main.cpp
@@ -6,16 +6,76 @@
 #include "Main.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
+#pragma link "AdvGrid"
+#pragma link "AdvObj"
+#pragma link "BaseGrid"
+#pragma link "cxButtons"
+#pragma link "cxGraphics"
+#pragma link "cxLookAndFeelPainters"
+#pragma link "cxLookAndFeels"
+#pragma link "dxSkinBlack"
+#pragma link "dxSkinBlue"
+#pragma link "dxSkinBlueprint"
+#pragma link "dxSkinCaramel"
+#pragma link "dxSkinCoffee"
+#pragma link "dxSkinDarkRoom"
+#pragma link "dxSkinDarkSide"
+#pragma link "dxSkinDevExpressDarkStyle"
+#pragma link "dxSkinDevExpressStyle"
+#pragma link "dxSkinFoggy"
+#pragma link "dxSkinGlassOceans"
+#pragma link "dxSkinHighContrast"
+#pragma link "dxSkiniMaginary"
+#pragma link "dxSkinLilian"
+#pragma link "dxSkinLiquidSky"
+#pragma link "dxSkinLondonLiquidSky"
+#pragma link "dxSkinMcSkin"
+#pragma link "dxSkinMetropolis"
+#pragma link "dxSkinMetropolisDark"
+#pragma link "dxSkinMoneyTwins"
+#pragma link "dxSkinOffice2007Black"
+#pragma link "dxSkinOffice2007Blue"
+#pragma link "dxSkinOffice2007Green"
+#pragma link "dxSkinOffice2007Pink"
+#pragma link "dxSkinOffice2007Silver"
+#pragma link "dxSkinOffice2010Black"
+#pragma link "dxSkinOffice2010Blue"
+#pragma link "dxSkinOffice2010Silver"
+#pragma link "dxSkinOffice2013DarkGray"
+#pragma link "dxSkinOffice2013LightGray"
+#pragma link "dxSkinOffice2013White"
+#pragma link "dxSkinPumpkin"
+#pragma link "dxSkinsCore"
+#pragma link "dxSkinsDefaultPainters"
+#pragma link "dxSkinSeven"
+#pragma link "dxSkinSevenClassic"
+#pragma link "dxSkinSharp"
+#pragma link "dxSkinSharpPlus"
+#pragma link "dxSkinSilver"
+#pragma link "dxSkinSpringTime"
+#pragma link "dxSkinStardust"
+#pragma link "dxSkinSummer2008"
+#pragma link "dxSkinTheAsphaltWorld"
+#pragma link "dxSkinValentine"
+#pragma link "dxSkinVisualStudio2013Blue"
+#pragma link "dxSkinVisualStudio2013Dark"
+#pragma link "dxSkinVisualStudio2013Light"
+#pragma link "dxSkinVS2010"
+#pragma link "dxSkinWhiteprint"
+#pragma link "dxSkinXmas2008Blue"
 #pragma resource "*.dfm"
 TFormMain *FormMain;
 //---------------------------------------------------------------------------
 __fastcall TFormMain::TFormMain(TComponent* Owner)
 	: TForm(Owner)
 {
+	Init_Tetris();
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TFormMain::Init_Tetris() {
+	// SETTING START PAGE
+	Notebook_Main->PageIndex = 0; // LOGIN
 
 }
 //---------------------------------------------------------------------------
@@ -28,6 +88,13 @@ void __fastcall TFormMain::PrintMsg(UnicodeString _str) {
 void __fastcall TFormMain::btn_EnterClick(TObject *Sender)
 {
 	Notebook_Main->PageIndex = 1; // Lobby
+}
+//---------------------------------------------------------------------------
+
+
+void __fastcall TFormMain::btn_GAMEClick(TObject *Sender)
+{
+	Notebook_Main->PageIndex = 2; // GAME
 }
 //---------------------------------------------------------------------------
 
--- Tetris_Client/Main.h
+++ Tetris_Client/Main.h
@@ -8,6 +8,65 @@
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
 #include <Vcl.ExtCtrls.hpp>
+#include "AdvGrid.hpp"
+#include "AdvObj.hpp"
+#include "BaseGrid.hpp"
+#include <Vcl.Grids.hpp>
+#include "cxButtons.hpp"
+#include "cxGraphics.hpp"
+#include "cxLookAndFeelPainters.hpp"
+#include "cxLookAndFeels.hpp"
+#include "dxSkinBlack.hpp"
+#include "dxSkinBlue.hpp"
+#include "dxSkinBlueprint.hpp"
+#include "dxSkinCaramel.hpp"
+#include "dxSkinCoffee.hpp"
+#include "dxSkinDarkRoom.hpp"
+#include "dxSkinDarkSide.hpp"
+#include "dxSkinDevExpressDarkStyle.hpp"
+#include "dxSkinDevExpressStyle.hpp"
+#include "dxSkinFoggy.hpp"
+#include "dxSkinGlassOceans.hpp"
+#include "dxSkinHighContrast.hpp"
+#include "dxSkiniMaginary.hpp"
+#include "dxSkinLilian.hpp"
+#include "dxSkinLiquidSky.hpp"
+#include "dxSkinLondonLiquidSky.hpp"
+#include "dxSkinMcSkin.hpp"
+#include "dxSkinMetropolis.hpp"
+#include "dxSkinMetropolisDark.hpp"
+#include "dxSkinMoneyTwins.hpp"
+#include "dxSkinOffice2007Black.hpp"
+#include "dxSkinOffice2007Blue.hpp"
+#include "dxSkinOffice2007Green.hpp"
+#include "dxSkinOffice2007Pink.hpp"
+#include "dxSkinOffice2007Silver.hpp"
+#include "dxSkinOffice2010Black.hpp"
+#include "dxSkinOffice2010Blue.hpp"
+#include "dxSkinOffice2010Silver.hpp"
+#include "dxSkinOffice2013DarkGray.hpp"
+#include "dxSkinOffice2013LightGray.hpp"
+#include "dxSkinOffice2013White.hpp"
+#include "dxSkinPumpkin.hpp"
+#include "dxSkinsCore.hpp"
+#include "dxSkinsDefaultPainters.hpp"
+#include "dxSkinSeven.hpp"
+#include "dxSkinSevenClassic.hpp"
+#include "dxSkinSharp.hpp"
+#include "dxSkinSharpPlus.hpp"
+#include "dxSkinSilver.hpp"
+#include "dxSkinSpringTime.hpp"
+#include "dxSkinStardust.hpp"
+#include "dxSkinSummer2008.hpp"
+#include "dxSkinTheAsphaltWorld.hpp"
+#include "dxSkinValentine.hpp"
+#include "dxSkinVisualStudio2013Blue.hpp"
+#include "dxSkinVisualStudio2013Dark.hpp"
+#include "dxSkinVisualStudio2013Light.hpp"
+#include "dxSkinVS2010.hpp"
+#include "dxSkinWhiteprint.hpp"
+#include "dxSkinXmas2008Blue.hpp"
+#include <Vcl.Menus.hpp>
 //---------------------------------------------------------------------------
 class TFormMain : public TForm
 {
@@ -18,7 +77,21 @@ __published:	// IDE-managed Components
 	TPanel *_pnBase_03_Game;
 	TPanel *_pnBase_02_Lobby;
 	TButton *btn_Enter;
+	TAdvStringGrid *grid_Mine;
+	TAdvStringGrid *grid_P1;
+	TAdvStringGrid *grid_P2;
+	TAdvStringGrid *grid_P3;
+	TAdvStringGrid *grid_P4;
+	TAdvStringGrid *grid_P5;
+	TPanel *pn_Pause;
+	TAdvStringGrid *grid_Items;
+	TImage *img_NextBlock;
+	TMemo *memo;
+	TcxButton *btn_LogOut;
+	TButton *btn_START;
+	TButton *btn_GAME;
 	void __fastcall btn_EnterClick(TObject *Sender);
+	void __fastcall btn_GAMEClick(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
 	__fastcall TFormMain(TComponent* Owner);
