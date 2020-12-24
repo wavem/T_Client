1adb6e7b08ceecab40ddafc06a963752f1e8382c
wavem <fenix24@naver.com>
2020-12-25 03:24:06 +0900

    [Test] Create Random Item
    - Random Item 생성되는 것 까지 확인.
    - 이제 어떻게 써먹을 것이냐.....는 다음 커밋에서부터.

--- Tetris_Client/GameLogic.cpp
+++ Tetris_Client/GameLogic.cpp
@@ -58,11 +58,73 @@ void __fastcall TFormMain::CheckCombo() {
 		return;
 	}
 	AddScore(m_CleardLineCnt * m_ComboCnt);
-	m_ComboCnt++;
+	//if(++m_ComboCnt > 1) {
+		CreateRandomItem();
+	//}
 	lb_Combo->Caption = m_ComboCnt;
 	m_OldScore = m_Score;
 	m_CleardLineCnt = 0;
-	//CreateRandomItem();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TFormMain::CreateRandomItem() {
+
+	PrintChat_InGame(L"CREATE RANDOM ITEM");
+
+	///***** COMMON INIT *****///
+	BYTE t_Byte = 0;
+	int t_TopLine = MAX_GRID_Y - 1;
+	bool t_bFindComplete = false;
+	int total_block_cnt = 0;
+	int check_cnt = 0;
+	int randNum = 0;
+
+
+
+	///***** FIND TOP LINE *****///
+	for(int y = 0 ; y < MAX_GRID_Y ; y++) {
+		for(int x = 0 ; x < MAX_GRID_X ; x++) {
+			if(GetBitStatus(m_MyView[x][y], 7) || GetBitStatus(m_MyView[x][y], 6)) continue;
+			t_Byte = GetBlockData(m_MyView[x][y]);
+			if(t_Byte != 0) {
+				t_TopLine = y;
+				t_bFindComplete = true;
+				break;
+			}
+		}
+		if(t_bFindComplete) break;
+	}
+
+	if(!t_bFindComplete) return; // YOU DO NOT DESERVED GET ITEM
+
+
+	///***** GET TOTAL BLOCK NUMBER *****///
+	for(int y = 0 ; y < MAX_GRID_Y ; y++) {
+		for(int x = 0 ; x < MAX_GRID_X ; x++) {
+			if(GetBitStatus(m_MyView[x][y], 7) || GetBitStatus(m_MyView[x][y], 6)) continue;
+			t_Byte = GetBlockData(m_MyView[x][y]);
+			if(t_Byte != 0) total_block_cnt++;
+		}
+	}
+
+	///***** GET RANDOM NUMBER *****///
+	randNum = rand() % total_block_cnt;
+
+
+	///***** GET ITEM *****///
+	for(int y = 0 ; y < MAX_GRID_Y ; y++) {
+		for(int x = 0 ; x < MAX_GRID_X ; x++) {
+			if(GetBitStatus(m_MyView[x][y], 7) || GetBitStatus(m_MyView[x][y], 6)) continue;
+			t_Byte = GetBlockData(m_MyView[x][y]);
+			if(t_Byte != 0) {
+				if(check_cnt == randNum) {
+                    m_MyView[x][y] = TYPE_ITEM_PLUS;
+					return;
+				}
+				check_cnt++;
+			}
+		}
+	}
 }
 //---------------------------------------------------------------------------
 
